9091d5a01d397e57fd2a072ec31d8a04948b3da8
a.motehari <a.motehari@ASHKANMOTEHARI->
2024-03-13 16:37:07 +0330

    Add project files.

---
+++ example/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num1 = { 1, 2,3,4,5};
+            int[] num2 = {1,4,6 };
+
+            //1,4
+            //6
+            //2,3,5
+
+
+            var t = num1.Except(num2).ToList(); // delete
+            var t1= num2.Except(num1).ToList(); // add
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
